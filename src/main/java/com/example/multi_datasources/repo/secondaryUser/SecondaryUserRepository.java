2afc565b5e84377af2bf02ded12e710c52e34710
Phuong <buiphuongaptutc@gmail.com>
2022-12-27 11:47:26 +0700

    feat: tests

--- src/main/java/com/example/multi_datasources/repo/secondaryUser/SecondaryUserRepository.java
+++ src/main/java/com/example/multi_datasources/repo/secondaryUser/SecondaryUserRepository.java
@@ -4,4 +4,5 @@ import com.example.multi_datasources.model.secondary.SecondaryUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SecondaryUserRepository extends JpaRepository<SecondaryUser, Long> {
+    int countByName(String name);
 }
--- src/main/java/com/example/multi_datasources/services/InsertUserServiceImpl.java
+++ src/main/java/com/example/multi_datasources/services/InsertUserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.multi_datasources.services;
 
+import com.example.multi_datasources.dto.UserCountDto;
 import com.example.multi_datasources.dto.UsersDto;
 import com.example.multi_datasources.model.PublicUser;
 import com.example.multi_datasources.model.secondary.SecondaryUser;
@@ -46,8 +47,24 @@ public class InsertUserServiceImpl implements InsertUserService {
         SecondaryUser sUSer;
         if (StringUtils.isNotEmpty(dto.getSecondaryUser().getName())) {
             sUSer = secondaryUserRepo.save(dto.getSecondaryUser());
-        } else throw new Exception("insertInvalidUsers: Name parameter must be present in users to save. Rolling back transaction.");
+        } else
+            throw new Exception("insertInvalidUsers: Name parameter must be present in users to save. Rolling back transaction.");
 
         return new UsersDto(pUser, sUSer);
+    }
+
+    @Transactional
+    public UsersDto insertUsers(UsersDto dto) {
+        PublicUser pUser = publicUserRepo.save(dto.getPublicUser());
+        SecondaryUser sUser = secondaryUserRepo.save(dto.getSecondaryUser());
+        return new UsersDto(pUser, sUser);
+    }
+
+    @Override
+    public UserCountDto countByName(String name) {
+        return UserCountDto.builder()
+                .publicUser(publicUserRepo.countByName(name))
+                .secondaryUser(secondaryUserRepo.countByName(name))
+                .build();
     }
 }
